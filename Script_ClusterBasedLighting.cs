5ed3be8bfa5ec19e477f1a9e7fa94782b1b380bf
wsqjny <663436456@qq.com>
2019-07-04 22:58:15 +0800

    ComputeClusterAABB, DebugCluster

--- Script_ClusterBasedLighting.cs
+++ Script_ClusterBasedLighting.cs
@@ -1,7 +1,30 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+
+
+struct CD_DIM
+{
+    public float fieldOfViewY;
+    public float zNear;
+    public float zFar;
+
+    public float sD;
+    public float logDimY;
+    public float logDepth;
+
+    public int clusterDimX;
+    public int clusterDimY;
+    public int clusterDimZ;
+    public int clusterDimXYZ;
+};
+
+struct AABB
+{
+    public Vector4 Min;
+    public Vector4 Max;
+};
 
 [ExecuteInEditMode]
 #if UNITY_5_4_OR_NEWER
@@ -9,27 +32,126 @@ using UnityEngine;
 #endif
 public class Script_ClusterBasedLighting : MonoBehaviour
 {
+    public ComputeShader cs_ComputeClusterAABB;
+
+    public Material mtlDebugCluster;
+
+    private ComputeBuffer cb_ClusterAABBs;
+
     private RenderTexture _rtColor;
     private RenderTexture _rtDepth;
 
+    private Camera _camera;
+
+    private CD_DIM m_DimData;
+    private int m_ClusterGridBlockSize = 64;
+ 
 
     void Start()
     {
+        _camera = Camera.main;
+        
         _rtColor = new RenderTexture(Screen.width, Screen.height, 24);
         _rtDepth = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth, RenderTextureReadWrite.Linear);
+
+
+        CalculateMDim(_camera);
+
+        int stride = Marshal.SizeOf(typeof(AABB));
+        cb_ClusterAABBs = new ComputeBuffer(m_DimData.clusterDimXYZ, stride);
+
+        Pass_ComputeClusterAABB();
     }
-  
-    void Update()
-    {
-        
-    }
+
 
     void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
     {
         Graphics.SetRenderTarget(_rtColor.colorBuffer, _rtDepth.depthBuffer);
         GL.Clear(true, true, Color.gray);
 
+        Pass_DebugCluster();
+
         Graphics.Blit(_rtColor, destTexture);
     }
 
+
+    void Pass_ComputeClusterAABB()
+    {
+        var projectionMatrix = GL.GetGPUProjectionMatrix(_camera.projectionMatrix, false);
+        var projectionMatrixInvers = projectionMatrix.inverse;
+        cs_ComputeClusterAABB.SetMatrix("_InverseProjectionMatrix", projectionMatrixInvers);
+
+        UpdateClusterCBuffer(cs_ComputeClusterAABB);
+
+        int threadGroups = Mathf.CeilToInt(m_DimData.clusterDimXYZ / 1024.0f);
+
+        int kernel = cs_ComputeClusterAABB.FindKernel("CSMain");
+        cs_ComputeClusterAABB.SetBuffer(kernel, "RWClusterAABBs", cb_ClusterAABBs);
+        cs_ComputeClusterAABB.Dispatch(kernel, threadGroups, 1, 1);
+
+        AABB[] output = new AABB[100];
+        cb_ClusterAABBs.GetData(output);
+
+        //  Debug.Log(output);
+    }
+
+    void Pass_DebugCluster()
+    {
+        GL.wireframe = true;
+
+        mtlDebugCluster.SetBuffer("ClusterAABBs", cb_ClusterAABBs);
+
+        mtlDebugCluster.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Points, m_DimData.clusterDimXYZ);
+        
+        GL.wireframe = false;
+    }
+
+    void UpdateClusterCBuffer(ComputeShader cs)
+    {
+        int[] gridDims = { m_DimData.clusterDimX, m_DimData.clusterDimY, m_DimData.clusterDimZ };
+        int[] sizes = { m_ClusterGridBlockSize, m_ClusterGridBlockSize };
+        Vector4 screenDim = new Vector4((float)Screen.width, (float)Screen.height, 1.0f / Screen.width, 1.0f / Screen.height);
+        float viewNear = m_DimData.zNear;
+
+        cs.SetInts("ClusterCB_GridDim", gridDims);
+        cs.SetFloat("ClusterCB_ViewNear", viewNear);
+        cs.SetInts("ClusterCB_Size", sizes);
+        cs.SetFloat("ClusterCB_NearK", 1.0f + m_DimData.sD);
+        cs.SetFloat("ClusterCB_LogGridDimY", m_DimData.logDimY);
+        cs.SetVector("ClusterCB_ScreenDimensions", screenDim);
+    }
+
+    void CalculateMDim(Camera cam)
+    {
+        // The half-angle of the field of view in the Y-direction.
+        float fieldOfViewY = cam.fieldOfView * Mathf.Deg2Rad * 0.5f;//Degree 2 Radiance:  Param.CameraInfo.Property.Perspective.fFovAngleY * 0.5f;
+        float zNear = cam.nearClipPlane;// Param.CameraInfo.Property.Perspective.fMinVisibleDistance;
+        float zFar = cam.farClipPlane;// Param.CameraInfo.Property.Perspective.fMaxVisibleDistance;
+
+        // Number of clusters in the screen X direction.
+        int clusterDimX = Mathf.CeilToInt(Screen.width / (float)m_ClusterGridBlockSize);
+        // Number of clusters in the screen Y direction.
+        int clusterDimY = Mathf.CeilToInt(Screen.height / (float)m_ClusterGridBlockSize);
+
+        // The depth of the cluster grid during clustered rendering is dependent on the 
+        // number of clusters subdivisions in the screen Y direction.
+        // Source: Clustered Deferred and Forward Shading (2012) (Ola Olsson, Markus Billeter, Ulf Assarsson).
+        float sD = 2.0f * Mathf.Tan(fieldOfViewY) / (float)clusterDimY;
+        float logDimY = 1.0f / Mathf.Log(1.0f + sD);
+
+        float logDepth = Mathf.Log(zFar / zNear);
+        int clusterDimZ = Mathf.FloorToInt(logDepth * logDimY);
+
+        m_DimData.zNear = zNear;
+        m_DimData.zFar = zFar;
+        m_DimData.sD = sD;
+        m_DimData.fieldOfViewY = fieldOfViewY;
+        m_DimData.logDepth = logDepth;
+        m_DimData.logDimY = logDimY;
+        m_DimData.clusterDimX = clusterDimX;
+        m_DimData.clusterDimY = clusterDimY;
+        m_DimData.clusterDimZ = clusterDimZ;
+        m_DimData.clusterDimXYZ = clusterDimX * clusterDimY * clusterDimZ;
+    }
 }
---
+++ Shader_DebugCluster.shader
@@ -0,0 +1,161 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "ClusterBasedLightingGit/Shader_DebugCluster"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex main_VS
+			#pragma fragment main_PS
+			#pragma geometry main_GS
+			#pragma target 5.0
+			#pragma enable_d3d11_debug_symbols
+
+			#include "UnityCG.cginc"
+
+			struct VertexShaderOutput
+			{
+				float4 Min          : AABB_MIN;  // Min vertex position in view space.
+				float4 Max          : AABB_MAX;  // Max vertex position in view space.
+				float4 Color        : COLOR;     // Cluster color.
+			};
+
+			struct GeometryShaderOutput
+			{
+				float4 Color        : COLOR;
+				float4 Position     : SV_POSITION;          // Clip space position.
+			};
+
+			struct AABB
+			{
+				float4 Min;
+				float4 Max;
+			};
+
+			StructuredBuffer<AABB> ClusterAABBs;// : register(t1);
+
+			bool CMin(float3 a, float3 b)
+			{
+				if (a.x < b.x && a.y < b.y && a.z < b.z)
+					return true;
+				else
+					return false;
+			}
+
+			bool CMax(float3 a, float3 b)
+			{
+				if (a.x > b.x && a.y > b.y && a.z > b.z)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			float4 WorldToProject(float4 posWorld)
+			{	
+				float4 posVP0 = UnityObjectToClipPos(posWorld);
+				return posVP0;
+			}
+
+			VertexShaderOutput main_VS(uint VertexID : SV_VertexID)
+			{
+				uint clusterID = VertexID; ;// UniqueClusters[VertexID];// VertexID;
+
+				VertexShaderOutput vsOutput = (VertexShaderOutput)0;
+
+				AABB aabb = ClusterAABBs[clusterID];// ClusterAABBs[VertexID];
+
+				vsOutput.Min = aabb.Min;
+				vsOutput.Max = aabb.Max;
+
+				float4 factor = aabb.Max - aabb.Min;
+				factor *= 0.2;
+				vsOutput.Max = aabb.Min + factor;
+				vsOutput.Color = float4(1,1,1,1);
+
+				return vsOutput;
+			}
+
+
+			// Geometry shader to convert AABB to cube.
+			[maxvertexcount(16)]
+			void main_GS(point VertexShaderOutput IN[1], inout TriangleStream<GeometryShaderOutput> OutputStream)
+			{
+				float4 min = IN[0].Min;
+				float4 max = IN[0].Max;
+
+				// Clip space position
+				GeometryShaderOutput OUT = (GeometryShaderOutput)0;
+
+				// AABB vertices
+				const float4 Pos[8] = {
+					float4(min.x, min.y, min.z, 1.0f),    // 0
+					float4(min.x, min.y, max.z, 1.0f),    // 1
+					float4(min.x, max.y, min.z, 1.0f),    // 2
+
+					float4(min.x, max.y, max.z, 1.0f),    // 3
+					float4(max.x, min.y, min.z, 1.0f),    // 4
+					float4(max.x, min.y, max.z, 1.0f),    // 5
+					float4(max.x, max.y, min.z, 1.0f),    // 6
+					float4(max.x, max.y, max.z, 1.0f)     // 7
+				};
+
+				// Colors (to test correctness of AABB vertices)
+				const float4 Col[8] = {
+					float4(0.0f, 0.0f, 0.0f, 1.0f),       // Black
+					float4(0.0f, 0.0f, 1.0f, 1.0f),       // Blue
+					float4(0.0f, 1.0f, 0.0f, 1.0f),       // Green
+					float4(0.0f, 1.0f, 1.0f, 1.0f),       // Cyan
+					float4(1.0f, 0.0f, 0.0f, 1.0f),       // Red
+					float4(1.0f, 0.0f, 1.0f, 1.0f),       // Magenta
+					float4(1.0f, 1.0f, 0.0f, 1.0f),       // Yellow
+					float4(1.01, 1.0f, 1.0f, 1.0f)        // White
+				};
+
+				const uint Index[18] = {
+					0, 1, 2,
+					3, 6, 7,
+					4, 5, -1,
+					2, 6, 0,
+					4, 1, 5,
+					3, 7, -1
+				};
+
+				[unroll]
+				for (uint i = 0; i < 18; ++i)
+				{
+					if (Index[i] == (uint) - 1)
+					{
+						OutputStream.RestartStrip();
+					}
+					else
+					{
+						OUT.Position = WorldToProject(Pos[Index[i]]);
+						OUT.Color = IN[0].Color;
+						OutputStream.Append(OUT);
+					}
+				}
+			}
+
+			float4 main_PS(GeometryShaderOutput IN) : SV_Target
+			{
+				return IN.Color;
+			}
+
+			ENDCG
+		}
+	}
+}
